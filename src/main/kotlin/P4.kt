3b50f9b5a6018162a3429a0af7ac39e2eb43ccd5
Jordan Foo <foo.jordan@gmail.com>
2020-12-03 22:49:26 -0800

    Parts and defs

--- src/main/kotlin/P4.kt
+++ src/main/kotlin/P4.kt
@@ -1,63 +1,59 @@
 fun main() {
+    fun bounds(value: Int, min: Int, max: Int) {
+        if (value < min) {
+            error("min: $value $min")
+        }
+        if (value > max) {
+            error("max: $value $max")
+        }
+    }
+
     val records = load("P4.txt").joinToString(separator = " ").split("  ")
-    val requiredKeys = setOf(
-        "byr",
-        "iyr",
-        "eyr",
-        "hgt",
-        "hcl",
-        "ecl",
-        "pid",
+    val required = mapOf(
+        "byr" to fun(s: String) { bounds(s.toInt(), 1920, 2002) },
+        "iyr" to fun(s: String) { bounds(s.toInt(), 2010, 2020) },
+        "eyr" to fun(s: String) { bounds(s.toInt(), 2020, 2030) },
+        "hgt" to fun(s: String) {
+            val hgt = s.substring(0, s.length - 2).toInt()
+            if (s.endsWith("cm")) {
+                bounds(hgt, 150, 193)
+            } else if (s.endsWith("in")) {
+                bounds(hgt, 59, 76)
+            }
+        },
+        "hcl" to fun(s: String) {
+            if (!Regex("#[0-9a-f]{6}").matches(s)) {
+                error("not matches: $s")
+            }
+        },
+        "ecl" to fun(s: String) {
+            if (!setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(s)) {
+                error("cl: $s")
+            }
+        },
+        "pid" to fun(s: String) {
+            if (s.length != 9) {
+                error("length")
+            }
+        },
     )
 
-    var allReq = 0
     var valid = 0
     records.forEach {
-        val parts = it.trim().split(" ")
-        val partsMap = mutableMapOf<String, String>()
-        parts.forEach {
-            val (key, value) = it.split(":")
-            if (key != "cid") {
-                partsMap[key] = value
+        val parts = it.split(" ")
+        try {
+            val keys = mutableSetOf<String>()
+            parts.forEach { part ->
+                val (key, value) = part.split(":")
+                required[key]?.invoke(value)
+                keys.add(key)
             }
-        }
-        if (partsMap.keys.containsAll(requiredKeys)) {
-            allReq++
-            try {
-                bounds(partsMap["byr"]!!.toInt(), 1920, 2002)
-                bounds(partsMap["iyr"]!!.toInt(), 2010, 2020)
-                bounds(partsMap["eyr"]!!.toInt(), 2020, 2030)
-                val hgtValue = partsMap["hgt"]!!
-                val hgt = hgtValue.substring(0, hgtValue.length - 2).toInt()
-                if (hgtValue.endsWith("cm")) {
-                    bounds(hgt, 150, 193)
-                } else if (hgtValue.endsWith("in")) {
-                    bounds(hgt, 59, 76)
-                }
-                if (!Regex("#[0-9a-f]{6}").matches(partsMap["hcl"]!!)) {
-                    error("not matches: ${partsMap["hcl"]!!}")
-                }
-                if (!setOf("amb", "blu", "brn", "gry", "grn", "hzl", "oth").contains(partsMap["ecl"]!!)) {
-                    error("cl: ${partsMap["ecl"]}")
-                }
-                if (partsMap["pid"]!!.length != 9) {
-                    error("length")
-                }
+            if(keys.containsAll(required.keys)) {
                 valid++
-            } catch (e: Exception) {
-//                println(e.message)
             }
+        } catch (e: Exception) {
+//            println(e)
         }
     }
-    println(allReq)
     println(valid)
-}
-
-fun bounds(value: Int, min: Int, max: Int) {
-    if (value < min) {
-        error("min: $value $min")
-    }
-    if (value > max) {
-        error("max: $value $max")
-    }
 }
