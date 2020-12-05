ec3324d094dff0422d578b8893a3a129e33841c1
Jordan Foo <foo.jordan@gmail.com>
2020-12-04 21:18:19 -0800

    P5

--- src/main/kotlin/P5.kt
+++ src/main/kotlin/P5.kt
@@ -1,4 +1,25 @@
 fun main() {
-  val lines: List<String> = load("P5.txt")
-  println(lines)
+    val lines: List<String> = load("P5.txt")
+    var max = Integer.MIN_VALUE
+    val seats = mutableSetOf<Int>()
+    lines.forEach {
+        val rowString = it.substring(0, 7)
+        val row = Integer.parseInt(rowString.replace("F", "0").replace("B", "1"), 2)
+        val seatString = it.substring(7, 10)
+        val seat = Integer.parseInt(seatString.replace("L", "0").replace("R", "1"), 2)
+        val seatId = 8 * row + seat
+        seats.add(seatId)
+        if (seatId > max) {
+            max = seatId
+        }
+    }
+    println(max)
+    var i = 1
+    while(true){
+        if(seats.contains(i-1) && seats.contains(i+1) && !seats.contains(i)){
+            println(i)
+            return
+        }
+        i++
+    }
 }
