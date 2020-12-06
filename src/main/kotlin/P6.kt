700fa4e67c24068a33c416307b6a9cec2c775efa
Jordan Foo <foo.jordan@gmail.com>
2020-12-05 22:37:49 -0800

    refactoring

--- src/main/kotlin/P6.kt
+++ src/main/kotlin/P6.kt
@@ -7,7 +7,9 @@ fun main() {
     var sum2 = 0L
     lines.forEach { line ->
         val raw = line.split(" ")
-        sum2 += raw.first().filter { char -> raw.all { it.contains(char) } }.length
+        var intersect = raw.first().toHashSet()
+        raw.forEach { intersect = intersect.intersect(it.toHashSet()).toHashSet() }
+        sum2 += intersect.size
     }
     println(sum2)
 }
