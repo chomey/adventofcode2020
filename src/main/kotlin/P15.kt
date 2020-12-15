82c2e27364230f8fbaed9c97a203e962cd985b16
Jordan Foo <foo.jordan@gmail.com>
2020-12-14 22:06:19 -0800

    p15

---
+++ src/main/kotlin/P15.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val input = "1,20,8,12,0,14"
+    val vals = input.split(",").map { it.toInt() }
+
+    var counter = 0
+    val lastSaidTurns = mutableMapOf<Int, Int>()
+    var lastSaid = 0
+    while (counter < 30000000) {
+        if (counter < vals.size) {
+            if (counter > 0) {
+                lastSaidTurns[lastSaid] = counter - 1
+            }
+            lastSaid = vals[counter]
+        } else {
+            val lastSaidTurn = lastSaidTurns[lastSaid]
+            if (lastSaidTurn == null) {
+                lastSaidTurns[lastSaid] = counter - 1
+                lastSaid = 0
+            } else {
+                lastSaidTurns[lastSaid] = counter - 1
+                lastSaid = counter - 1 - lastSaidTurn
+            }
+        }
+        counter++
+    }
+    println(lastSaid)
+}
